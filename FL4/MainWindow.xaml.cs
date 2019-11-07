e324cf019d685c65bc3858c58fa51862ac28a0a5
eriobe <erik.oberg@miun.se>
2019-11-07 14:58:10 +0100

    Föreläsning 4 Yatzy

---
+++ FL4/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FL4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculateBmi_Click(object sender, RoutedEventArgs e)
+        {
+            double height = double.Parse(txtHeight.Text), mass = double.Parse(txtMass.Text);
+            double bmi;
+
+            bmi = mass / (height * height);
+            bmi = mass / Math.Pow(height, 2);
+            bmi = 25.5;
+            int number = 4;
+
+            int value = (int)bmi; // cast
+
+            value = 5 / 4;  // heltalsdivision
+            bmi = 5 / 4.0; // variant 1
+            int remainder = 5 % 4;
+            bmi = 5 / (double)number; // variant 2
+            txtResult.Text = remainder.ToString();
+
+            txtResult.Text = Math.Round(bmi,0).ToString();
+            if (bmi < 18.5)
+            {
+                // Underviktig
+            }
+            else if (bmi >= 18.5 && bmi <= 25)
+            {
+                // Normalvikt
+            }
+            else
+            {
+                // Övervikt
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double price = double.Parse(txtMass.Text);
+            int area = 57;
+
+            double pricePerSquare = price / area;
+
+            txtResult.Text = $"{pricePerSquare} kr/kvm";
+
+        }
+    }
+}
---
+++ Yatzy/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Yatzy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // En slumpgenerator vid namn random
+        Random random = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void BtnRollDices_Click(object sender, RoutedEventArgs e)
+        {
+            int dieOne, dieTwo, dieThree, dieFour, dieFive, totalSum, bonus;
+            // uppdrag
+            // kasta en tärning och presentera resultatet 
+
+            dieOne = random.Next(1, 7); // slumpa fram ett tal mellan 1 och 6
+            dieTwo = random.Next(1, 7); // slumpa fram ett tal mellan 1 och 6
+            dieThree = random.Next(1, 7); // slumpa fram ett tal mellan 1 och 6
+            dieFour = random.Next(1, 7); // slumpa fram ett tal mellan 1 och 6
+            dieFive = random.Next(1, 7); // slumpa fram ett tal mellan 1 och 6
+
+            // Uppdrag Presentera resultatet av slumpgeneratorn
+            // alla tärningar ska visas i gränssnittet
+            lblDieOne.Content = dieOne; 
+            lblDieTwo.Content = dieTwo; 
+            lblDieThree.Content = dieThree; 
+            lblDieFour.Content = dieFour;
+            lblDieFive.Content = dieFive;
+
+            // Beräkna totalsumman av alla tärningar
+            totalSum = dieOne + dieTwo + dieThree + dieFour + dieFive;
+            //totalSum = 62; // hårdkodat testdata
+            // uppdrag 2. Får man bonus?
+
+            if (totalSum >= 63)
+            {
+                bonus = 50;
+                totalSum += bonus;
+            }
+            else
+            {
+                bonus = 0;
+            }
+            txtBonus.Text = bonus.ToString();
+            txtTotal.Text = totalSum.ToString();
+
+        }
+    }
+}
